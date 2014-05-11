70332334db798e30a6ed31ea47df1f8bd3ec10d0
incrediblesolv <admin@issis.co.za>
2014-05-11 17:10:25 +0200

    JML1.5-LibraryDataStore-0.01j.php
    joomla code to set up plugin - needs more work.
    This is the LibraryDataStore plugin to be built and to be added to the special &copy 'knowtree' wordpress theme
    I want to make the installation more interactive so that librarians can add thier own custon fields
    and add to the extensive listing to be autmatically installed and used with an optional bar code scanner

---
+++ JML1.5-LibraryDataStore-0.01j.php
@@ -0,0 +1,34 @@
+<!--Joomla 1.5-->
+
+<?xml version="1.0" encoding="utf-8"?>
+ <!DOCTYPE install PUBLIC "-//Joomla! 1.5//DTD plugin 1.0//EN" "http://www.joomla.org/xml/dtd/1.5/plugin-install.dtd">
+
+<install version="1.5" type="plugin" group="search">
+
+<name>LibraryDataStore</name>
+<creationDate>Creation date</creationDate>
+<authorHilary Albutt </author>
+<authorEmail>admin \at/ issi |s| dot co dot za</authorEmail>
+<authorUrl>http://www.issis.co.za</authorUrl>
+<copyright>Copyright Hilary Albutt</copyright>
+<license>License,GNU/GPL v3</license>
+<version>0.01</version>
+<description>joomla code to set up plugin - needs more work.
+This is the LibraryDataStore plugin to be built and to be added to the special &copy 'knowtree' wordpress theme
+I want to make the installation more interactive so that librarians can add thier own custon fields
+and add to the extensive listing to be autmatically installed and used with an optional bar code scanner
+
+</description>
+
+<files>
+   <filename plugin="nameofplugin">LibraryDataStore.php</filename>
+</files>
+<languages>
+   <language tag="en-GB">language/en-GB/en-GB.LibraryDataStore.ini</language>
+</languages>
+
+<params>
+   <param name="paramname" type="typeofparameter" default="defaultsetting" label="title" description="description"/>
+</params>
+
+</install>
